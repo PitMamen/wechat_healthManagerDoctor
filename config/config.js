751a89883d3ee02400d1220075753da39fc2825a
向侠 <xia.xiang@bitvalue.com.cn>
2023-08-17 11:09:58 +0800

    搜索诊断添加一个参数

--- config/config.js
+++ config/config.js
@@ -5,3 +5,7 @@ export const baseURL = `http://192.168.1.121`;
 /**演示环境**/
 // export const SDKAPPID = 1400684981;
 // export const baseURL = `https://ys.mclouds.org.cn`;
+
+/**正式环境**/
+// export const SDKAPPID = 1400547247;
+// export const baseURL = `https://hmg.mclouds.org.cn`;
