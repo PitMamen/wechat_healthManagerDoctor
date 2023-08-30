2f12ae8f9b55ef319a645293898a8e5aa53add3e
tangjie <tangjie@gitee.com>
2023-08-30 19:08:28 +0800

    Do functions v2.0.0

--- pages2/pages/cash/pack.vue
+++ pages2/pages/cash/pack.vue
@@ -1,37 +1,59 @@
 <template>
 	<view class="wrap">
-		<view class="head">
-			<u-search
-				placeholder="输入文章标题进行搜索"
-				v-model="value"
-				:show-action="false"
-				@change="change"
-			></u-search>
-		</view>
-		<view class="content">
-			<u-empty mode="data" style="padding-top: 300rpx;" icon="/pages2/static/img/icon_nodata.png" v-if="list.length === 0"></u-empty>
-			<scroll-view class="list" :scroll-y="true" @scrolltolower="scrolltolower" v-else>
-				<view class="item" v-for="item in list" :key="item.articleId">
-					<view class="top" @click="viewHandler(item)">
-						<view class="row title">
-							<image src="/pages2/static/static/images/group/icon_note.png"></image>
-							<text>{{ item.title || '' }}</text>
-						</view>
-						<view class="row desc">{{ item.brief || '暂无' }}</view>
-						<view class="read">
-							<image src="/pages2/static/static/images/group/icon_read.png"></image>
-							<text>{{ item.clickNum || 0 }}</text>
-						</view>
-						<image class="abs" :src="item.previewUrl"></image>
-					</view>
-					<view class="bottom">
-						<view class="btn" @click="sendHandler(item)">
-							<image src="/pages2/static/static/images/group/icon_send.png"></image>
-							<text>发送给患者</text>
-						</view>
-					</view>
+		<view class="top">
+			<view class="title">钱包余额</view>
+			<view class="cash">￥2300.00</view>
+			<view class="block">
+				<view class="left">
+					<view class="line line1">可提现金额</view>
+					<view class="line line2">￥2000.00</view>
 				</view>
-			</scroll-view>
+				<view class="right">
+					<view class="line line1">待结算金额</view>
+					<view class="line line2">￥2000.00</view>
+				</view>
+			</view>
+			<view class="action">
+				<view class="btn" @click="goCashOut">提现</view>
+			</view>
+		</view>
+		<view class="bottom">
+			<view class="row" @click="goCashOutList">
+				<view class="contain">
+					<view class="left">
+						<image src="/static/static/images/cash/pack-1.png"></image>
+						<view class="name">提现记录</view>
+					</view>
+					<u-icon name="arrow-right" color="#1A1A1A" size="36rpx"></u-icon>
+				</view>
+			</view>
+			<view class="row" @click="goCashCard">
+				<view class="contain">
+					<view class="left">
+						<image src="/static/static/images/cash/pack-2.png"></image>
+						<view class="name">银行卡管理</view>
+					</view>
+					<u-icon name="arrow-right" color="#1A1A1A" size="36rpx"></u-icon>
+				</view>
+			</view>
+			<view class="row" @click="goCashTradeList">
+				<view class="contain">
+					<view class="left">
+						<image src="/static/static/images/cash/pack-3.png"></image>
+						<view class="name">交易记录</view>
+					</view>
+					<u-icon name="arrow-right" color="#1A1A1A" size="36rpx"></u-icon>
+				</view>
+			</view>
+			<view class="row" @click="goCashPwd">
+				<view class="contain">
+					<view class="left">
+						<image src="/static/static/images/cash/pack-4.png"></image>
+						<view class="name">支付密码管理</view>
+					</view>
+					<u-icon name="arrow-right" color="#1A1A1A" size="36rpx"></u-icon>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -40,91 +62,36 @@
 	export default {
 		data() {
 			return {
-				flag: false,
-				total: 0,
-				pageNo: 1,
-				value: '',
-				list: []
 			}
 		},
 		onLoad() {
-			this.getList();
 		},
 		onReady() {
 		},
 		onShow() {
 		},
 		methods: {
-			viewHandler(item) {
+			goCashOut() {
 				uni.navigateTo({
-					url: `/pages2/pages/group/note-info?id=${item.articleId}&title=${item.title}`
+					url: '/pages2/pages/cash/out'
 				});
 			},
-			sendHandler(item) {
-				const pages = getCurrentPages();
-				if (pages.length > 1){
-					const page = pages[pages.length - 1 - 1];
-					if (page.route==='pages2/pages/TUI-Chat-Group/chat' || page.route==='pages2/pages/TUI-Chat-Group2/chat'){
-						page.$vm.sendCustomMessage({
-							detail: {
-								payload: {
-									data: JSON.stringify({
-										content: item.title,
-										description: '文章卡',
-										id: item.articleId,
-										type: 'CustomArticleMessage'
-									}),
-									extension: '',
-									description: '文章卡'
-								}
-							}
-						});
-						uni.navigateBack({
-							delta: 1
-						});
-					}
-				}
-			},
-			change() {
-				this.value = this.value.trim();
-				this.pageNo = 1;
-				this.total = 0;
-				this.list = [];
-				this.getList();
-			},
-			getList() {
-				uni.showLoading({
-					title:'正在加载'
-				});
-				uni.$u.http.get(`/health-api/health/patient/allArticlesNewPage`, {
-					params: {
-						status: 2,
-						title: this.value,
-						pageSize: 10,
-						start: this.pageNo
-					}
-				}).then(res => {
-					res.data = res.data || {};
-					res.data.list = res.data.list || [];
-					res.data.total = res.data.total || 0;
-					this.total = res.data.total;
-					this.list = this.list.concat(res.data.list);
-				}).finally(() => {
-					this.flag = false;
-					uni.hideLoading();
+			goCashOutList() {
+				uni.navigateTo({
+					url: '/pages2/pages/cash/out-list'
 				});
 			},
-			scrolltolower() {
-				if (this.pageNo*10 >= this.total){
-					return;
-				}
-				if (this.flag){
-					return;
-				}
-				this.flag = true;
-				this.pageNo ++;
-				this.getList();
-			}
+			goCashCard() {
+				uni.navigateTo({
+					url: '/pages2/pages/cash/card'
+				});
+			},
+			goCashTradeList() {
+				uni.navigateTo({
+					url: '/pages2/pages/cash/trade-list'
+				});
+			},
+			goCashPwd() {}
 		}
 	}
 </script>
@@ -132,104 +99,106 @@
 <style lang="scss">
 	.wrap {
 		min-height: 100vh;
-		background: #FFFFFF;
-		.head {
-			position: fixed;
-			top: 0;
-			width: 100%;
-			padding: 30rpx 24rpx;
-			z-index: 1;
+		background: #F5F5F5;
+		.top {
+			margin: 0 24rpx;
+			padding: 40rpx 0;
 			background: #FFFFFF;
-			box-sizing: border-box;
+			border-radius: 4rpx;
+			.title {
+				margin-bottom: 18rpx;
+				font-size: 24rpx;
+				font-weight: 400;
+				color: #999999;
+				line-height: 44rpx;
+				text-align: center;
+			}
+			.cash {
+				margin-bottom: 32rpx;
+				font-size: 48rpx;
+				font-weight: 400;
+				color: #1A1A1A;
+				line-height: 72rpx;
+				text-align: center;
+			}
+			.block {
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				margin-bottom: 52rpx;
+				.left {
+					width: 50%;
+				}
+				.right {
+					width: 50%;
+					border-left: 1rpx solid #CCCCCC;
+				}
+				.line {
+					text-align: center;
+					&.line1 {
+						font-size: 24rpx;
+						font-weight: 400;
+						color: #999999;
+						line-height: 36rpx;
+					}
+					&.line2 {
+						font-size: 30rpx;
+						font-weight: 400;
+						color: #1A1A1A;
+						line-height: 50rpx;
+					}
+				}
+			}
+			.action {
+				padding: 0 110rpx;
+				.btn {
+					font-size: 28rpx;
+					font-weight: 400;
+					color: #FFFFFF;
+					line-height: 68rpx;
+					text-align: center;
+					background: #3894FF;
+					border-radius: 34rpx;
+				}
+			}
 		}
-		.content {
-			margin-top: calc(60rpx + 32px);
-			.list {
-				max-height: calc(100vh - 60rpx - 32px);
-				background: #F2F2F2;
-				.item {
-					margin-bottom: 20rpx;
-					background: #FFFFFF;
-					&:first-child {
-						margin-top: 20rpx;
+		.bottom {
+			margin: 30rpx 24rpx 0 24rpx;
+			background: #FFFFFF;
+			border-radius: 4rpx;
+			.row {
+				padding: 0 32rpx 0 28rpx;
+				&:last-child {
+					.contain {
+						border-bottom: none;
 					}
-					.top {
-						position: relative;
-						padding: 28rpx 24rpx;
-						.row {
-							max-width: 540rpx;
-							white-space: nowrap;
-							overflow: hidden;
-							text-overflow: ellipsis;
-						}
-						.title {
-							font-size: 30rpx;
-							font-weight: 500;
-							color: #4D4D4D;
-							line-height: 36rpx;
-							image {
-								width: 28rpx;
-								height: 30rpx;
-								margin-right: 15rpx;
-								padding: 3rpx 0;
-								vertical-align: middle;
-							}
-							text {
-								vertical-align: middle;
-							}
-						}
-						.desc {
-							margin-top: 24rpx;
-							font-size: 28rpx;
-							font-weight: 400;
-							color: #999999;
-							line-height: 34rpx;
-						}
-						.read {
-							margin-top: 24rpx;
-							font-size: 22rpx;
-							font-weight: 400;
-							color: #999999;
-							line-height: 26rpx;
-							image {
-								width: 26rpx;
-								height: 26rpx;
-								margin-right: 10rpx;
-								vertical-align: middle;
-							}
-							text {
-								vertical-align: text-bottom;
-							}
-						}
-						.abs {
-							position: absolute;
-							top: 28rpx;
-							right: 24rpx;
-							width: 140rpx;
-							height: 140rpx;
-						}
+				}
+				.contain {
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+					border-bottom: 1rpx solid #CCCCCC;
+				}
+				.left {
+					display: flex;
+					align-items: center;
+					justify-content: flex-start;
+					image {
+						width: 30rpx;
+						height: 30rpx;
+						margin-right: 15rpx;
 					}
-					.bottom {
-						padding: 30rpx 0;
-						border-top: 1rpx solid #E6E6E6;
-						.btn {
-							text-align: center;
-							image {
-								display: inline-block;
-								width: 33rpx;
-								height: 31rpx;
-								margin-right: 10rpx;
-								vertical-align: middle;
-							}
-							text {
-								font-size: 28rpx;
-								font-weight: 400;
-								color: #4D4D4D;
-								line-height: 31rpx;
-								vertical-align: middle;
-							}
-						}
+					.name {
+						font-size: 30rpx;
+						font-weight: 400;
+						color: #4D4D4D;
+						line-height: 108rpx;
 					}
+				}
+				>.u-icon {
+					position: relative;
+					top: 2rpx;
+					margin-left: 2rpx;
 				}
 			}
 		}
