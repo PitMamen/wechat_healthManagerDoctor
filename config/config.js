69da199b683fb5ef0f137eae1820ef33c204099c
pxk <541931879@qq.com>
2023-12-18 13:52:49 +0800

    f

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 /**测试环境**/
-export const SDKAPPID = 1600015255 ;//1400613243
-export const baseURL = `http://develop.mclouds.org.cn:8009`;
-export const CAURL = `https://realnameverify-test05.fadada.com`;
-export const showDataLog = true;
-export const SocketUrl='ws://192.168.1.121:8091/webSocket/'
+// export const SDKAPPID = 1600015255 ;//1400613243
+// export const baseURL = `http://develop.mclouds.org.cn:8009`;
+// export const CAURL = `https://realnameverify-test05.fadada.com`;
+// export const showDataLog = true;
+// export const SocketUrl='ws://192.168.1.121:8091/webSocket/'
 
 /**演示环境**/
 // export const SDKAPPID = 1400684981;
@@ -13,8 +13,8 @@ export const SocketUrl='ws://192.168.1.121:8091/webSocket/'
 // export const SocketUrl='ws://192.168.1.121:8091/webSocket/'
 
 /**正式环境**/
-// export const SDKAPPID = 1400547247;
-// export const baseURL = `https://hmg.mclouds.org.cn`;
-// export const CAURL = `https://realnameverify05.fadada.com`;
-// export const showDataLog = false;
-// export const SocketUrl='wss://hmg.mclouds.org.cn/medical-api/webSocket/'
+export const SDKAPPID = 1400547247;
+export const baseURL = `https://hmg.mclouds.org.cn`;
+export const CAURL = `https://realnameverify05.fadada.com`;
+export const showDataLog = false;
+export const SocketUrl='wss://hmg.mclouds.org.cn/medical-api/webSocket/'
